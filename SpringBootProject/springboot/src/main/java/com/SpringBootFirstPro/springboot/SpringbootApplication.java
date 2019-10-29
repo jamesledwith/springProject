bef27a4ef0a85da80dda53fa23fee1262ac5b00b
James Ledwith <jamesledwith@live.ie>
2019-10-28 20:42:46 -0700

    Autoconfiguration
    auto config basics, where to find and what beans are created

--- SpringBootProject/springboot/src/main/java/com/SpringBootFirstPro/springboot/SpringbootApplication.java
+++ SpringBootProject/springboot/src/main/java/com/SpringBootFirstPro/springboot/SpringbootApplication.java
@@ -2,12 +2,21 @@ package com.SpringBootFirstPro.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		ApplicationContext applicationContext =  
+				SpringApplication.run(SpringbootApplication.class, args);
+		
+		//gets all names of beans in the applicationcontext
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+		
+		
 	}
 
 }
